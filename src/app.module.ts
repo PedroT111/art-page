9690ca95e807c313dbfc87b5fc22835ad7fc130b
pedrot111 <pedrochincolla11@gmail.com>
2024-04-22 10:48:17 +0200

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/user.module';
+import { ArtWorkModule } from './art-work/art-work.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    DatabaseModule,
+    UserModule,
+    ArtWorkModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
