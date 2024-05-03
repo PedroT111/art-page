2fe0609e0b0e59f5d7ec89157ee4bc7fb53d90b0
pedrot111 <pedrochincolla11@gmail.com>
2024-05-03 15:17:55 +0200

    implements interceptor and validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ResponseInterceptor } from './common/exceptions/response-interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
