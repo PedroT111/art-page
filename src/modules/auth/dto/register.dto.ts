62d3434c7e49453dd20b6db0ee09ac2dabe5018a
pedrot111 <pedrochincolla11@gmail.com>
2024-05-03 15:24:52 +0200

    add validators to dto

--- src/modules/auth/dto/register.dto.ts
+++ src/modules/auth/dto/register.dto.ts
@@ -1,7 +1,17 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class RegisterDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+  @IsString()
+  @IsNotEmpty()
   lastName: string;
+  @IsString()
+  @IsNotEmpty()
   email: string;
+  @IsString()
+  @IsNotEmpty()
   password: string;
   role: string;
 }
--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,7 +1,17 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+  @IsNotEmpty()
+  @IsString()
   lastName: string;
+  @IsNotEmpty()
+  @IsString()
   email: string;
+  @IsNotEmpty()
+  @IsString()
   password: string;
   role: string;
 }
